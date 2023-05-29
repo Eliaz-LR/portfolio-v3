5478c479c17aa8f9567f90e41449b62a766ef455
Eliaz Le Riche <47760515+Eliaz-LR@users.noreply.github.com>
2023-05-29 23:23:23 +0200

    Head + other small stuff

--- src/components/Bio.vue
+++ src/components/Bio.vue
@@ -2,29 +2,31 @@
   <p class="text-center">
     👋<br />
     Hi, I'm Eliaz, a {{ age }} years-old developer who is curently studying at
-    <a href='https://www.iut-clermont.fr/formations/dut-informatique/'>UCA</a>.
+    <a href="https://www.iut-clermont.fr/formations/dut-informatique/">UCA</a>.
     <br />
-    I'm interested in a lot of subjects ranging from economics to technology in general.<br />
+    I'm interested in a lot of subjects ranging from economics to technology in
+    general.<br />
     I'm open to offers if you're looking for a full-stack developer. 😄<br />
-    <a href="https://github.com/Eliaz-LR">My Github</a>. <a href="mailto:me@eliaz-lr.dev">Email me here</a> !
+    <a href="https://github.com/Eliaz-LR">My Github</a>.
+    <a href="mailto:me@eliaz-lr.dev">Email me here</a> !
   </p>
 </template>
 
 <script>
-import dayjs from 'dayjs'
+import dayjs from "dayjs";
 
 export default {
   data() {
     return {
-      age: {}
-    }
+      age: {},
+    };
   },
   created() {
     this.interval = setInterval(() => {
-      this.age = (dayjs().diff(dayjs('2000-12-03'), 'year', true)).toFixed(9);
-    }, 50)
-  }
-}
+      this.age = dayjs().diff(dayjs("2000-12-03"), "year", true).toFixed(9);
+    }, 50);
+  },
+};
 </script>
 
 <style scoped>
