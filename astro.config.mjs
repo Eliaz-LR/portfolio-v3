e3edd5a36f0ad9750127157114087e95005491cb
Eliaz Le Riche <47760515+Eliaz-LR@users.noreply.github.com>
2023-06-11 10:28:31 +0200

    Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,17 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   experimental: {
-    assets: true,
+    assets: true
   },
-  integrations: [
-    tailwind({
-      config: { applyBaseStyles: false },
-    }),
-    vue(),
-  ],
-  site: "https://portfolio--v3.web.app/",
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), vue(), sitemap()],
+  site: "https://portfolio--v3.web.app/"
 });
