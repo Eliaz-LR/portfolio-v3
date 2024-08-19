a2830c95f4b72a66edd723af0058975bc285ce37
Eliaz LR <47760515+Eliaz-LR@users.noreply.github.com>
2024-08-19 10:46:39 +0200

    Update main.ts

--- src/scripts/cookie/main.ts
+++ src/scripts/cookie/main.ts
@@ -7,6 +7,8 @@ var ws = new WebSocket(`ws://127.0.0.1:8000/ws/${client_id}`);
 // );
 
 var hit_count = 0;
+var local_hit_count = get_local_storage();
+var local_num_clicks: NodeListOf<Element>;
 var click_display: NodeListOf<Element>;
 var connected_count = 0;
 var connected_people: NodeListOf<Element>;
@@ -20,9 +22,7 @@ function handle_message(event: MessageEvent) {
   switch (data.type) {
     case "hit_count":
       hit_count = data.number;
-      click_display.forEach((element) => {
-        element.setAttribute("data-tip", hit_count.toString());
-      });
+      render_hit_count();
       break;
     case "connected_count":
       connected_count = data.number;
@@ -34,25 +34,43 @@ function handle_message(event: MessageEvent) {
 }
 function on_cookie_click(event: MouseEvent) {
   animatePlusOneOnCookieClick(event);
+  local_hit_count += 1;
+  render_hit_count();
   ws.send(
     JSON.stringify({
       type: "click",
     })
   );
+  set_local_storage(local_hit_count);
+}
+
+function render_hit_count() {
+  click_display.forEach((element) => {
+    element.setAttribute("data-tip", hit_count.toString());
+  });
+  local_num_clicks.forEach((element) => {
+    element.textContent = ((100 * local_hit_count) / hit_count).toFixed(5);
+  });
+}
+
+function set_local_storage(number: number) {
+  localStorage.setItem("hit_count", number.toString());
+}
+
+function get_local_storage(): number {
+  var value = localStorage.getItem("hit_count");
+  if (value) {
+    hit_count = parseInt(value);
+    return hit_count;
+  }
+  return 0;
 }
 
 function initBinding() {
   console.log("init binding");
 
-  const ws_id = document.querySelectorAll("#ws-id");
-  ws_id.forEach((element) => {
-    element.textContent = client_id.toString();
-  });
-
   click_display = document.querySelectorAll("#click-tip-display");
-  click_display.forEach((element) => {
-    element.setAttribute("data-tip", hit_count.toString());
-  });
+  local_num_clicks = document.querySelectorAll("#local-num-clicks");
 
   connected_people = document.querySelectorAll("#connected-people");
   connected_people.forEach((element) => {
@@ -61,11 +79,9 @@ function initBinding() {
 
   const cookie = document.querySelectorAll("#cookie");
   cookie.forEach((element) => {
-    const clickHandler = (event: MouseEvent) => {
-      event.preventDefault();
+    element.addEventListener("click", (event: MouseEvent) => {
       on_cookie_click(event);
-    };
-    element.addEventListener("click", clickHandler);
+    });
   });
 }
 
